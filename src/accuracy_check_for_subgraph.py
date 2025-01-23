cf6e6c8cd462b60fb46fd38943cf6316127a4dfb
Alep, Fazreilie <fazreilie.alep@intel.com>
2025-01-23 15:16:37 +0800

    update results

--- src/accuracy_check_for_subgraph.py
+++ src/accuracy_check_for_subgraph.py
@@ -144,7 +144,6 @@ if __name__ == "__main__":
     
     # check with npu subgraphs (all nodes)
     output_folder = os.getenv('OUTPUT_FOLDER_NPU')
-    accuracy_check_for_subgraph_all(subgraph_folder_npu, subgraph_folder_npu, output_folder, tol, dp)
-    # subgraph_files = [  "OpenVINO-EP-subgraph_19.xml",
-    #                     "OpenVINO-EP-subgraph_21.xml" ]
-    # accuracy_check_for_subgraph(subgraph_folder_npu, subgraph_folder_npu, subgraph_files, output_folder, tol, dp)
+    # accuracy_check_for_subgraph_all(subgraph_folder_npu, subgraph_folder_npu, output_folder, tol, dp)
+    subgraph_files = ["OpenVINO-EP-subgraph_25.xml"]
+    accuracy_check_for_subgraph(subgraph_folder_npu, subgraph_folder_npu, subgraph_files, output_folder, tol, dp)
